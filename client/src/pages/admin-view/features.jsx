3098aa5d244ad3f0a0ce2808d2ee5d4114bc7e2a
Subhajit Roy <subhajitroy@11122000>
2025-01-27 20:59:43 +0530

    All Changes Added

---
+++ client/src/pages/admin-view/features.jsx
@@ -0,0 +1,9 @@
+
+
+
+
+export default function AdminFeatures(){
+    return (
+        <div>Admin Features</div>
+    )
+}
---
+++ client/src/pages/admin-view/orders.jsx
@@ -0,0 +1,12 @@
+import AdminOrdersView from "@/components/admin-view/orders";
+
+
+
+
+export default function AdminOrders(){
+    return (
+        <div>
+            <AdminOrdersView/>
+        </div>
+    )
+}
